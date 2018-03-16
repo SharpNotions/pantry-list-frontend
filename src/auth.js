f1785701827d945c0b7ff9c69f07aa2821ec1084
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-03-16 17:32:29 -0400

    refactor: Cleanup unused async await

--- src/auth.js
+++ src/auth.js
@@ -1,14 +1,12 @@
 import Cookie from 'js-cookie'
 
 export default {
-  async authorize (token) {
+  authorize (token) {
     const inTwentyFourHours = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
     Cookie.set('id_token', token, {expires: inTwentyFourHours})
-    return Promise.resolve(this.isAuthorized())
   },
-  async unauthorize () {
+  unauthorize () {
     Cookie.remove('id_token')
-    return Promise.resolve(this.isAuthorized())
   },
   isAuthorized () {
     return !!Cookie.get('id_token')
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -22,8 +22,6 @@ export default {
     }
     if (!window.gapi) {
       return;
-    } else {
-      this.gapi = window.gapi;
     }
 
     gapi.signin2.render('g-signin2', {
@@ -32,23 +30,12 @@ export default {
     });
   },
   methods: {
-    async login () {
-      try {
-        await auth.authorize()
-        this.$router.push('/')
-      } catch (error) {
-        this.error = 'We ran into an error while attempting to log you in.'
-      }
-    },
     onSignIn (user) {
       const hd = user.getHostedDomain();
       if (hd === 'sharpnotions.com') {
         var id_token = user.getAuthResponse().id_token;
         auth.authorize(id_token);
         this.$router.replace(this.$route.query.redirect || '/')
-      } else {
-        auth.unauthorize();
-        this.gapi.auth2.getAuthInstance().disconnect();
       }
     }
   }
