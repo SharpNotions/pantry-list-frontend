ba307639456a6daec9a243afa6f9ffcb0f95457b
Adam Mac <atom.mac@gmail.com>
2018-03-17 15:04:34 -0400

    feat: Add ability to add list items.

---
+++ src/components/ItemAdder.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="list">
+    <input
+      type="text"
+      @keyup.enter="submit()"
+      v-model="newLabel"
+    />
+    <button
+      :disabled="canSubmit"
+      @keyPress.enter="submit()"
+      @click="submit()">+
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'item-adder',
+  props: {
+    addItem: Function
+  },
+  data () {
+    return {
+      newLabel: ''
+    }
+  },
+  computed: {
+    canSubmit () {
+      return !this.newLabel.length
+    }
+  },
+  methods: {
+    async submit () {
+      await this.addItem(
+        this.newLabel
+      )
+      this.newLabel = ''
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
