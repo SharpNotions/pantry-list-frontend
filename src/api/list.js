d865e4c2af74becf219ddaff1369ddc23f44e7d4
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-03-26 09:18:19 -0400

    refactor: No need to send both auth headers and cookies

--- src/api/list.js
+++ src/api/list.js
@@ -1,11 +1,6 @@
-import Cookie from 'js-cookie'
-
 export default {
   listItems () {
     return fetch('/api/items', {
-      headers: {
-        authorization: `Bearer ${Cookie.get('id_token')}`
-      },
       credentials: 'include'
     }).then(response => response.json())
   }
