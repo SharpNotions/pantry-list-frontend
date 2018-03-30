1d3bf9cf5df5fec2fa73755cdfb55d43f77e9225
Ryan Chatterton <ryan.chatterton@sharpnotions.com>
2018-03-30 10:46:32 -0400

    update build number output

--- scripts/ci/build_dockerfile.sh
+++ scripts/ci/build_dockerfile.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "create docker file"
-echo  -en "FROM sharpnotionsllc/pantry-list-frontend:${DRONE_BUILD_NUMBER} \nEXPOSE 80" &> Dockerfile
+echo  -en "FROM sharpnotionsllc/pantry-list-frontend:1.0.${DRONE_BUILD_NUMBER} \nEXPOSE 80" &> Dockerfile
 echo "DockerFile Created"
