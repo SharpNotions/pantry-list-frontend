30b2441dff6f16510ea86d520aebf2fd2e0e99b6
Ryan Chatterton <ryan.chatterton@sharpnotions.com>
2018-07-20 15:19:55 -0400

    Revert "feat(audit): Audit fix - webpack 4"

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var path = require('path')
 var webpack = require('webpack')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: ['babel-polyfill', './src/main.js'],
@@ -13,15 +12,26 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['vue-style-loader', 'css-loader']
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
       },
       {
         test: /\.scss$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /\.sass$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ]
       },
       {
         test: /\.vue$/,
@@ -31,8 +41,12 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            scss: ['vue-style-loader', 'css-loader', 'sass-loader'],
-            sass: [
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
               'vue-style-loader',
               'css-loader',
               'sass-loader?indentedSyntax'
@@ -62,7 +76,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      vue$: 'vue/dist/vue.esm.js',
+      'vue$': 'vue/dist/vue.esm.js',
       '@': path.resolve(__dirname, 'src')
     },
     extensions: ['*', '.js', '.vue', '.json']
@@ -82,9 +96,7 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map',
-  plugins: [new VueLoaderPlugin()],
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+  devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
