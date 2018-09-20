141df447db219af92ab2e830cfe2a4a350d614de
Adam Mac <atom.mac@gmail.com>
2018-09-19 21:55:18 -0400

    feat: Add Error404 component.

---
+++ src/components/Error404.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-layout align-center column>
+      <h1>Ah, Jeeze :frown:</h1>
+      <p align="center">
+        Looks like we couldn't find this page...
+      </p>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import auth from '@/auth'
+
+export default {
+  name: 'Error404'
+}
+</script>
+
+<style lang="scss">
+</style>
