df43736837fea72cf3212b569024b44a290cc3e4
Adam Mac <atom.mac@gmail.com>
2018-10-21 21:24:36 -0400

    feat: Add api/http module.

---
+++ src/api/http.js
@@ -0,0 +1,25 @@
+export default {
+  listeners: {
+    request: [],
+    response: []
+  },
+  clearInterceptors: function() {
+    this.listeners.request.splice(0, this.listeners.request.length)
+    this.listeners.response.splice(0, this.listeners.response.length)
+  },
+  addRequestInterceptor: function(listener) {
+    this.listeners.request.push(listener)
+  },
+  addResponseInterceptor: function(listener) {
+    this.listeners.response.push(listener)
+  },
+  fetch: async function(url, options) {
+    await Promise.all(
+      this.listeners.request.map(listener => listener(url, options))
+    )
+    const response = await fetch(url, options)
+    await Promise.all(
+      this.listeners.response.map(listener => listener(url, options, response))
+    )
+  }
+}
