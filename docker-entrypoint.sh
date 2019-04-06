eb102fc2a2e037383adcf0e023d14595c7f3d25d
Atom Mac <atom.mac@gmail.com>
2019-04-06 17:44:06 -0400

    chore: Remove commented build code.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 sed "s/\${PORT}/$PORT/" /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 cat /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
